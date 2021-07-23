6cf668a18828a3f6a71a72bfd27a3e16d010889a
TAHIR <tahirmama8@gmail.com>
2021-07-23 23:02:40 +0500

    Adding Css

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
 <hr/>
 <h2>Introduction</h2>
 
-    <Name name = "Tahir" F_name= "Muzaffar" age = "21" sports="Cricket & Volleyball" F_plan= "Leading Businessman"/>
-    <Name name = "Waseem" F_name= "Azam" age = "21" sports="Pubg" F_plan= "NAB Officer"/>
-    <Name name = "Hashim" F_name= "Raza" age = "24" sports="Badminton" F_plan= "Businessman"/>
-    <Name name = "Mufaddal" F_name= "Arif" age = "21" sports="Swimming" F_plan= "Biggest Engineer Of Country"/>
+    <Name name = "Tahir" F_name= "Muzaffar" age = "21" sports="Cricket & Volleyball" F_plan= "Leading Businessman" className="first"/>
+    <Name name = "Waseem" F_name= "Azam" age = "21" sports="Pubg" F_plan= "NAB Officer" className="second"/>
+    <Name name = "Hashim" F_name= "Raza" age = "24" sports="Badminton" F_plan= "Businessman" className="third"/>
+    <Name name = "Mufaddal" F_name= "Arif" age = "21" sports="Swimming" F_plan= "Biggest Engineer Of Country" className="fourth"/>
 
 
     </div>
