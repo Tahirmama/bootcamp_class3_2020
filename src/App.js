16f24d90a780cd0e2dd1b6ce7c7a30d9812a4282
TAHIR <tahirmama8@gmail.com>
2021-07-23 22:57:30 +0500

    Adding props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import Name from './Name'
 function App() {
   return (
     <div className="App">
+      <hr/>
     <h1>Using Props </h1>
+<hr/>
+<h2>Introduction</h2>
 
-    <Name name = "Tahir" F_name= "Muzaffar" age = "21" />
+    <Name name = "Tahir" F_name= "Muzaffar" age = "21" sports="Cricket & Volleyball" F_plan= "Leading Businessman"/>
+    <Name name = "Waseem" F_name= "Azam" age = "21" sports="Pubg" F_plan= "NAB Officer"/>
+    <Name name = "Hashim" F_name= "Raza" age = "24" sports="Badminton" F_plan= "Businessman"/>
+    <Name name = "Mufaddal" F_name= "Arif" age = "21" sports="Swimming" F_plan= "Biggest Engineer Of Country"/>
+
+
     </div>
 
 );
--- src/Name.js
+++ src/Name.js
@@ -3,10 +3,17 @@ import React from "react";
 function Name(props){
     return(
     <div>
-<h3>My name is {props.name}
+<h3>My name is : {props.name}
 <br/>
-My father name is {props.F_name}
-My age is {props.age}</h3>
+My father name is : {props.F_name}
+<br/>
+My age is : {props.age}
+<br/>
+My hobby is to play : {props.sports}
+<br/>
+My future goal is to become a {props.F_plan}
+
+</h3>
 
 
     </div>
